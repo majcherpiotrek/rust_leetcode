02e14e834f7c389df40f66aed09816cd05f01599
Piotr Majcher <majcherpiotr.dev@gmail.com>
2023-09-24 16:49:40 +0100

    small refactor

--- src/two_sum.rs
+++ src/two_sum.rs
@@ -3,13 +3,13 @@ use std::convert::TryFrom;
 // https://leetcode.com/problems/two-sum/
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let mut sorted = nums.iter().enumerate().collect::<Vec<(usize, &i32)>>();
-    sorted.sort_by(|(_, v_a), (_, v_b)| v_a.cmp(v_b));
+    sorted.sort_by(|(_, &v_a), (_, &v_b)| v_a.cmp(&v_b));
 
-    for (sorted_index, (i, num_a)) in sorted.iter().enumerate() {
+    for (sorted_index, (i, &num_a)) in sorted.iter().enumerate() {
         let slice_to_search = &sorted[sorted_index + 1..];
-        let search = target - *num_a;
+        let search = target - num_a;
         match slice_to_search
-            .binary_search_by(|elem| elem.1.cmp(&search))
+            .binary_search_by(|&(_, &v)| v.cmp(&search))
             .ok()
             .and_then(|index| slice_to_search.get(index))
             .and_then(|(j, _)| i32::try_from(*j).ok())
